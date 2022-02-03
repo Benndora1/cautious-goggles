1f5b93b490e8470e3323a91e2c86c8b7eb8d0e13
Benndora1 <Benndora2@gmail.com>
2022-02-03 15:24:07 +0300

    setup

--- Realestate/settings/base.py
+++ Realestate/settings/base.py
@@ -133,10 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'staticfiles/'
-STATIC_ROOT = BASE_DIR + '/staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIR = []
-MEDIA_URL = "/mediafiles"
-MEDIA_ROOT = BASE_DIR + '/mediafiles'
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 
 
