4ca51dbe20ffebb1377c933c40c897daa5b172c2
Benndora1 <Benndora2@gmail.com>
2022-02-04 12:13:19 +0300

    logs

--- Realestate/settings/base.py
+++ Realestate/settings/base.py
@@ -150,52 +150,44 @@ MEDIA_ROOT = BASE_DIR / 'mediafiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-import logging
-import logging.config
+# import logging
+# import logging.config
 
-from django.utils.log import DEFAULT_LOGGING
+# from django.utils.log import DEFAULT_LOGGING
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
-LOG_LEVEL = "INFO"
+# LOG_LEVEL = "INFO"
 
-logging.config.dictConfig({
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "console": {
-        "format": "%(asctime)s %(name)-12s %(levelname)-8s %(mesage)s",
-        },
+# logging.config.dictConfig(
+#     {
+#         "version": 1,
+#         "disable_existing_loggers": False,
+#         "formatters": {
+#             "console": {
+#                 "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+#             },
+#             "file": {"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
+#             "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
+#         },
+#         "handlers": {
+#             "console": {
+#                 "class": "logging.StreamHandler",
+#                 "formatter": "console",
+#             },
+#             "file": {
+#                 "level": "INFO",
+#                 "class": "logging.FileHandler",
+#                 "formatter": "file",
+#                 "filename": "logs/real_estate.log",
+#             },
+#             "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
+#         },
+#         "loggers": {
+#             "": {"level": "INFO", "handlers": ["console", "file"], "propagate": False},
+#             "apps": {"level": "INFO", "handlers": ["console"], "propagate": False},
+#             "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
+#         },
+#     }
+# )
 
-       "file": {"format": "%(asctime)s %(name)-12s %(levelname)-8s %(mesage)s"},   
-        "django,server": DEFAULT_LOGGING["formatters"]["django.server"],
-
-        
-        },
-
-        "handlers": {
-            "console": {
-                "class": "logging.StreamHandler",
-                "formatter": "console",
-            },
-
-            "file": {
-                "class": "logging.FileHandler",
-                "level": "INFO",
-                "formatter": "file",
-                "filename": "logs/realestate.log",
-            },
-            "django,server": DEFAULT_LOGGING["handlers"]["django.server"],
-      }, 
-
-        "loggers": {
-            "": {"level": "INFO", "handlers": ["console", "file"],"propagate": False
-            },
-        }
-            "apps": {
-                "level": "INFO",
-                "handlers": ["console" ],"propagate": False
-
-            },
-            "django,server": DEFAULT_LOGGING["handlers"]["django.server"],
-  })
