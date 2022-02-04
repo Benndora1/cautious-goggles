ab98faaa99b80f72918e33939a0c798884721a9c
Benndora1 <Benndora2@gmail.com>
2022-02-04 12:30:44 +0300

    database

--- Realestate/settings/development.py
+++ Realestate/settings/development.py
@@ -3,20 +3,19 @@ from .base import *
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
-#          'USER': 
-#          ''
-   
-   
 #     }
 # }
+DATABASES = {
+    "default": {
+        "ENGINE": env("POSTGRES_ENGINE"),
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("PG_HOST"),
+        "PORT": env("PG_PORT"),
+    }
+}
