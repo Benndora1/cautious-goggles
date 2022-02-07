4dfa85c6d4e917e1947a955e26661411f46e87c6
Benndora1 <Benndora2@gmail.com>
2022-02-07 15:23:06 +0300

    property serializer

---
+++ apps/properties/serializers.py
@@ -0,0 +1,59 @@
+from django_countries.serializer_fields import CountryField
+from django_countries.serializer import CountryFieldMixin
+from rest_framework import serializers
+from .models import Property, PropertyViews
+
+class PropertySerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+    country = CountryField(name_only=True)
+
+    class Meta:
+        model = Property
+        fields = [
+            "id",
+            "user",
+            "title",
+            "slug",
+            "ref_code",
+            "description",
+            "country", 
+            "city",
+            "postal_code",
+            "street_address",
+            "property_number",
+            "price",
+            "tax",
+            "final_property_price",
+            "plot_area"
+            "total_floors",
+            "bedrooms",
+            "bathrooms",
+            "advert_type"
+
+        ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
