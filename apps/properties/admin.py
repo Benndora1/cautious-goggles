e0d5e6c5a7fd1aaf8b9fa379e4c8214523c0f723
Benndora1 <Benndora2@gmail.com>
2022-02-08 17:19:30 +0300

    Properties

--- apps/properties/admin.py
+++ apps/properties/admin.py
@@ -1,19 +1,12 @@
 from django.contrib import admin
-from .models import PropertyViews, Property
+
+from .models import Property, PropertyViews
+
 
 class PropertyAdmin(admin.ModelAdmin):
-    list_display = [
-        "title",
-        "country", 
-        "advert_type",
-        "property_type"
-    ]
+    list_display = ["title", "country", "advert_type", "property_type"]
+    list_filter = ["advert_type", "property_type", "country"]
 
-    list_filter = [
-        "advert_type",
-        "property_type",
-        "country"
-    ]
 
-admin.site.register(PropertyAdmin, Property)
+admin.site.register(Property, PropertyAdmin)
 admin.site.register(PropertyViews)
