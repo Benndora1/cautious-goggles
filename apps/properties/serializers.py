1ee47bb8125056782c2a102b842b52f066572063
Benndora1 <Benndora2@gmail.com>
2022-02-08 18:06:01 +0300

    Properties

--- apps/properties/serializers.py
+++ apps/properties/serializers.py
@@ -1,11 +1,18 @@
 from django_countries.serializer_fields import CountryField
-from django_countries.serializer import CountryFieldMixin
+from django_countries.serializers import CountryFieldMixin
 from rest_framework import serializers
+
 from .models import Property, PropertyViews
+
 
 class PropertySerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
     country = CountryField(name_only=True)
+    cover_photo = serializers.SerializerMethodField()
+    photo1 = serializers.SerializerMethodField()
+    photo2 = serializers.SerializerMethodField()
+    photo3 = serializers.SerializerMethodField()
+    photo4 = serializers.SerializerMethodField()
 
     class Meta:
         model = Property
@@ -16,7 +23,7 @@ class PropertySerializer(serializers.ModelSerializer):
             "slug",
             "ref_code",
             "description",
-            "country", 
+            "country",
             "city",
             "postal_code",
             "street_address",
@@ -24,7 +31,7 @@ class PropertySerializer(serializers.ModelSerializer):
             "price",
             "tax",
             "final_property_price",
-            "plot_area"
+            "plot_area",
             "total_floors",
             "bedrooms",
             "bathrooms",
@@ -36,41 +43,37 @@ class PropertySerializer(serializers.ModelSerializer):
             "photo3",
             "photo4",
             "published_status",
-            "views"
-
+            "views",
         ]
 
     def get_user(self, obj):
         return obj.user.username
 
+    def get_cover_photo(self, obj):
+        return obj.cover_photo.url
+
+    def get_photo1(self, obj):
+        return obj.photo1.url
+
+    def get_photo2(self, obj):
+        return obj.photo2.url
+
+    def get_photo3(self, obj):
+        return obj.photo3.url
+
+    def get_photo4(self, obj):
+        return obj.photo4.url
+
+
 class PropertyCreateSerializer(serializers.ModelSerializer):
     country = CountryField(name_only=True)
 
     class Meta:
-        model: Property
+        model = Property
         exclude = ["updated_at", "pkid"]
 
 
 class PropertyViewSerializer(serializers.ModelSerializer):
     class Meta:
-        model : PropertyViews 
-        exclude = ["updated_at","pkid"]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        model = PropertyViews
+        exclude = ["updated_at", "pkid"]
