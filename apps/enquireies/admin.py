027fd24f4b55ee7d4a401a2c9508f6269a763013
Benndora1 <Benndora2@gmail.com>
2022-02-08 17:04:33 +0300

    Enquiry model

--- apps/enquireies/admin.py
+++ apps/enquireies/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+
+from .models import Enquiry
+
+class EnquiryAdmin(admin.ModelAdmin):
 # Register your models here.
+
+    list_display = ('name', 'email',  'message', 'phone_number', 'created_at')
+
+admin.site.register(Enquiry)
